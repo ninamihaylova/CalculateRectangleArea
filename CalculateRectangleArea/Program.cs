ab90ebec48e075c09bd707a30a700470e666524c
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-07-01 18:46:23 +0300

    Initial commit

---
+++ CalculateRectangleArea/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        
+        int width = int.Parse(Console.ReadLine());
+
+       
+        int length = int.Parse(Console.ReadLine());
+
+        int area = width * length;
+        Console.WriteLine(area);
+    }
+
+    static int CalculateRectangleArea(int width, int length)
+    {
+        return width * length;
+    }
+}
